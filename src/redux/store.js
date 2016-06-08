b8e03e7e9a3f5961f7aa911f90c988b04685a6d8
%%GITNAME%% <%%GITEMAIL%%>
2016-06-09 01:45:17 +0200

    Add redux middlewares and support for chrome redux dev tools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,12 @@
-import { createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
 import reducer from './modules/reducer';
+import logger from 'redux-logger';
+
+let finalCreateStore = compose(
+  applyMiddleware(logger()),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+)(createStore);
 
 export default function configureStore(initialState = {}) {
-  return createStore(reducer, initialState)
+  return finalCreateStore(reducer, initialState);
 }
