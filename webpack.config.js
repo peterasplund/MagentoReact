ef330e807e9b8df5d442deb0d0e6ae72c0971fc4
%%GITNAME%% <%%GITEMAIL%%>
2016-06-09 00:31:52 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    entry: ['./client/client.js'],
+    output: {
+        path: './dist',
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            },
+        ]
+    }
+}
